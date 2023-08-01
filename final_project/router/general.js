829fc3e3d684bd3fbe763380a349d6d4f5c4a5e4
TemposTunes <130808437+TemposTunes@users.noreply.github.com>
2023-07-31 22:51:38 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,20 +23,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let booksFiltered = books.filter((books) => books.author === author);
+  res.send(booksFiltered);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let booksFiltered = books.filter((books) => books.title === title);
+  res.send(booksFiltered);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
