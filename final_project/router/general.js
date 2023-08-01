dbe7e598e07da922ae56cbf741052a35d7e32eda
TemposTunes <130808437+TemposTunes@users.noreply.github.com>
2023-07-31 22:52:46 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,7 +18,7 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn])
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
@@ -38,7 +38,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn].reviews)
+  res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
